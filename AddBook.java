c7d1b15e8c39ed433529de4dd316e9b8ebc17002
Bhushan Harode <bhushanharode0246@gmail.com>
2023-12-25 11:19:39 +0530

    Add files via upload

--- AddBook.java
+++ AddBook.java
@@ -1,38 +1,49 @@
 package class_files;
-import java.awt.*;
-import javax.swing.*;
-import java.util.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 public class AddBook extends JFrame implements ActionListener{
     Random id=new Random();
-    int number=id.nextInt(999999);
-
-    JTextField tfname,tfauthor,tfpage,tfpublisher,tfprice,tfedition; 
+    int number=id.nextInt(99999999);
+    JLabel labelid;
+    JTextField tfname, tfauthor, tfpage, tfpublisher, tfprice, tfedition;
     JButton add,back;
 
     AddBook(){
         getContentPane().setBackground(Color.WHITE);//background is white
         setLayout(null);
+        setResizable(false);
 
-        JLabel heading=new JLabel("Add Book ");
-        heading.setBounds(340,35,500,50);
-        heading.setFont(new Font("SAN_SERIF",Font.BOLD,28));
+        JLabel heading=new JLabel("<html><p style='border-bottom: 3px solid rgb(0, 0, 0);'> Add New Book </p></html>");
+        heading.setBounds(280,35,500,50);
+        heading.setFont(new Font("Arial", Font.BOLD, 35));
         heading.setBackground(Color.WHITE);
         heading.setForeground(Color.BLACK);
         add(heading);
 
+
+        Font font = new Font("Arial", Font.BOLD,18);
+
         JLabel labelname=new JLabel("Name of the book:");
         labelname.setBounds(50,140,170,50);
-        labelname.setFont(new Font("serif",Font.PLAIN,18));
+        labelname.setFont(font);
         add(labelname);
 
-        tfname=new JTextField();
+        tfname = new JTextField();
         tfname.setBounds(230,150,180,30);
         add(tfname);
 
         JLabel labelauthor=new JLabel("Author name:");
         labelauthor.setBounds(50,190,170,50);
-        labelauthor.setFont(new Font("serif",Font.PLAIN,18));
+        labelauthor.setFont(font);
         add(labelauthor);
 
         tfauthor=new JTextField();
@@ -41,7 +52,7 @@ public class AddBook extends JFrame implements ActionListener{
 
         JLabel labelpage=new JLabel("No. of pages:");
         labelpage.setBounds(50,240,170,60);
-        labelpage.setFont(new Font("serif",Font.PLAIN,18));
+        labelpage.setFont(font);
         add(labelpage);
 
         tfpage =new JTextField();
@@ -50,7 +61,7 @@ public class AddBook extends JFrame implements ActionListener{
 
         JLabel labelpublisher=new JLabel("Publisher name:");
         labelpublisher.setBounds(50,290,170,60);
-        labelpublisher.setFont(new Font("serif",Font.PLAIN,18));
+        labelpublisher.setFont(font);
         add(labelpublisher);
 
         tfpublisher=new JTextField();
@@ -59,7 +70,7 @@ public class AddBook extends JFrame implements ActionListener{
 
         JLabel labelprice=new JLabel("Price:");
         labelprice.setBounds(50,340,170,60);
-        labelprice.setFont(new Font("serif",Font.PLAIN,18));
+        labelprice.setFont(font);
         add(labelprice);
 
         tfprice=new JTextField();
@@ -68,27 +79,28 @@ public class AddBook extends JFrame implements ActionListener{
 
         JLabel labeledition=new JLabel("Edition:");
         labeledition.setBounds(50,390,170,50);
-        labeledition.setFont(new Font("serif",Font.PLAIN,18));
+        labeledition.setFont(font);
         add(labeledition);
 
         tfedition=new JTextField();
         tfedition.setBounds(230,400,180,30);
-        add(tfedition); 
+        add(tfedition);
 
         JLabel labelbookid=new JLabel("Book Id:");
         labelbookid.setBounds(50,440,170,50);
-        labelbookid.setFont(new Font("serif",Font.PLAIN,18));
+        labelbookid.setFont(font);
         add(labelbookid);
 
-        JLabel labelid=new JLabel(""+ number);
+        labelid = new JLabel(""+ number);
         labelid.setBounds(230,440,170,50);
-        labelid.setFont(new Font("serif",Font.PLAIN,18));
+        labelid.setFont(font);
         add(labelid);
-         
+        
         add=new JButton("Add Book Details");
         add.setBounds(200,520,170,50);
         add.setFont(new Font("serif",Font.PLAIN,16));
         add.addActionListener(this);
+        add.setFocusPainted(false);
         add.setBackground(Color.BLACK);
         add.setForeground(Color.WHITE);
         add(add);
@@ -97,27 +109,29 @@ public class AddBook extends JFrame implements ActionListener{
         back.setBounds(420,520,170,50);
         back.setFont(new Font("serif",Font.PLAIN,16));
         back.addActionListener(this);
+        add.setFocusPainted(false);
         back.setBackground(Color.BLACK);
         back.setForeground(Color.WHITE);
 
-        setSize(800,650);
+        setSize(800,700);
         setLocation(300,50);
         setVisible(true);
         add(back);
     }
     public void actionPerformed(ActionEvent ae){
         if (ae.getSource() == add){
-            String name=tfname.getText();
-            String author=tfauthor.getText();     
-            String npages=tfpage.getText();  
-            String publishername=tfpublisher.getText(); 
-            String price=tfprice.getText();  
-            String edition=tfedition.getText();   
+            String bookName = tfname.getText();
+            String authName = tfauthor.getText();
+            String pages = tfpage.getText();
+            String publisher = tfpublisher.getText();
+            String price = tfprice.getText();
+            String edition = tfedition.getText();
             String bookId = labelid.getText();
+
             try{
-                Conn conn=new Conn();
+                Conn c=new Conn();
                 String query= "insert into bookdetails('"+bookName+"','"+authName+"','"+pages+"','"+publisher+"','"+price+"','"+edition+"','"+bookId+"')";
-                conn.s.executeUpdate(query);
+                c.s.executeUpdate(query);
                 JOptionPane.showMessageDialog(null, "Book Details Added Successfully");
                 setVisible(false);
                 new Dashboard();
@@ -125,12 +139,13 @@ public class AddBook extends JFrame implements ActionListener{
             e.printStackTrace();
             }
         }
+
         else{
-         setVisible(false);
+            setVisible(false);
+            new Dashboard();
         }
     }
     public static void main(String args[]){
         new AddBook();
     }
 }
-  
