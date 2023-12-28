91bc54a59a99e20226d10869f13563c099b1c316
Nidhi sikarwar <140337433+Nidhisikarwar@users.noreply.github.com>
2023-12-29 00:33:56 +0530

    Add files via upload

--- RemoveBook.java
+++ RemoveBook.java
@@ -10,49 +10,99 @@ public class RemoveBook extends JFrame implements ActionListener{
     Choice cbookname;
     JButton delete,back;
 
-
+//239, 250, 252
     RemoveBook(){
 
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         setResizable(false);
-
-        JLabel labelbookname=new JLabel("Book Name");
-        labelbookname.setBounds(50,50,100,30);
+        
+        JLabel labelheading=new JLabel("Remove Book");
+        labelheading.setBounds(280,30,240,40);
+        labelheading.setForeground(Color.BLACK);
+        labelheading.setFont(new Font("MONOSPACED",Font.BOLD,28));
+        add(labelheading);
+        
+        
+        JLabel labelbookname=new JLabel("Search by Book Name:");
+        labelbookname.setBounds(50,95,200,30);
         labelbookname.setForeground(Color.BLACK);
+        labelbookname.setFont(new Font("Arial",Font.PLAIN,18));
         add(labelbookname);
 
         cbookname=new Choice();
-        cbookname.setBounds(200,50,100,30); 
+        cbookname.setBounds(260,100,140,30); 
+        cbookname.setFont(new Font("Arial",Font.PLAIN,16));
         add(cbookname);  
+        
+        Font fontl = new Font("Arial",Font.PLAIN,17);
+        JLabel labeldetails=new JLabel("Your Book details are:");
+        labeldetails.setBounds(50,140,250,30);
+        labeldetails.setForeground(Color.BLACK);
+        labeldetails.setFont(new Font("Arial",Font.PLAIN,18));
+        add(labeldetails);
 
         JLabel labelbookid=new JLabel("Book Id");
-        labelbookid.setBounds(50,50,100,30);
+        labelbookid.setBounds(50,190,140,30);
         labelbookid.setForeground(Color.BLACK);
+        labelbookid.setFont(fontl);
         add(labelbookid);
 
         JLabel lblbookid=new JLabel();
-        lblbookid.setBounds(50,100,100,30);
+        lblbookid.setBounds(200,200,140,30);
         add(lblbookid);
 
         JLabel labelauthor=new JLabel("Author Name");
-        labelbookid.setBounds(50,50,100,30);
-        labelbookid.setForeground(Color.BLACK);
-        add(labelbookid);
+        labelauthor.setBounds(50,230,140,30);
+        labelauthor.setForeground(Color.BLACK);
+        labelauthor.setFont(fontl);
+        add(labelauthor);
 
         JLabel lblauthor=new JLabel();
-        lblauthor.setBounds(50,100,100,30);
+        lblauthor.setBounds(200,240,140,30);
         add(lblauthor);
         
         JLabel labelpublisher=new JLabel("Publisher Name");
-        labelpublisher.setBounds(50,50,100,30);
+        labelpublisher.setBounds(50,270,140,30);
         labelpublisher.setForeground(Color.BLACK);
+        labelpublisher.setFont(fontl);
         add(labelpublisher);
 
         JLabel lblpublisher=new JLabel();
-        lblpublisher.setBounds(50,100,100,30);
+        lblpublisher.setBounds(200,280,140,30);
         add(lblpublisher);
         
+        JLabel labelpages=new JLabel("Number of pages");
+        labelpages.setBounds(50,310,140,30);
+        labelpages.setForeground(Color.BLACK);
+        labelpages.setFont(fontl);
+        add(labelpages);
+
+        JLabel lblpages=new JLabel();
+        lblbookid.setBounds(200,320,140,30);
+        add(lblpages);
+        
+        JLabel labelprice=new JLabel("Price");
+        labelprice.setBounds(50,350,140,30);
+        labelprice.setForeground(Color.BLACK);
+        labelprice.setFont(fontl);
+        add(labelprice);
+
+        JLabel lblprice=new JLabel();
+        lblprice.setBounds(200,360,140,30);
+        add(lblprice);
+        
+        JLabel labeledition=new JLabel("Edition");
+        labeledition.setBounds(50,390,150,30);
+        labeledition.setForeground(Color.BLACK);
+        labeledition.setFont(fontl);
+        add(labeledition);
+
+        JLabel lbledition=new JLabel();
+        lbledition.setBounds(200,400,150,30);
+        add(lbledition);
+        
+       
 
            /* cbookname.addItemListener(new ItemListener() {
                 public void itemStateChanged(itemEvent ie) {
@@ -75,36 +125,107 @@ public class RemoveBook extends JFrame implements ActionListener{
             });
 */
             delete=new JButton("Delete");
-            delete.setBounds(220,300,100,30);
+            delete.setBounds(50,450,130,30);
+            delete.setFont(new Font("Arial",Font.PLAIN,16));
             delete.setBackground(Color.BLACK);
             delete.setForeground(Color.WHITE);
             delete.addActionListener(this);
             add(delete);
 
             back= new JButton("Back");
-            back.setBounds(220,300,100,30);
+            back.setBounds(230,450,130,30);
+            back.setFont(new Font("Arial",Font.PLAIN,16));
             back.setBackground(Color.BLACK);
             back.setForeground(Color.WHITE);
             back.addActionListener(this);
             add(back);
-
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("./images/book.png"));
-        Image i2 = i1.getImage().getScaledInstance(200,240, Image.SCALE_DEFAULT);
+            
+        /*ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("./icons/book.png"));
+        Image i2 = i1.getImage().getScaledInstance(300,140, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel img = new JLabel(new ImageIcon(i2));
-        img.setBounds(310,8,700,600);
-        add(img);
+        img.setBounds(310,8,400,200);
+        add(img);*/
 
-        setSize(1000,800);
+        
+
+        setSize(800,650);
         setLocation(300,150);
         setVisible(true);
         
     }
 
     public void actionPerformed(ActionEvent ae){
+         /* cbookname.addItemListener(new ItemListener() {
+                public void itemStateChanged(itemEvent ie) {
+            try{
+            Conn c=new Conn();
+            String query ="select * from bookdetails where bookName = '"+cbookname.getSelectedItem()+"'"; 
+            ResultSet rs= c.s.executeQuery(query);
+            while(rs.next()){
+                 //cbookname.add(rs.getString("bookName"));
+
+                lblbookid.setText(rs.getString("bookId"));
+                lblauthor.setText(rs.getString("authName"));
+                lblpublisher.setText(rs.getString("publisher"));
+               }
+            }
+            catch(Exception e) {
+            e.printStackTrace();
+            }
+            }
+            });
+*/
 
     }
     public static void main(String args[]){
         new RemoveBook();
     }
 }
+
+/*package class_files;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class RemoveBook extends JFrame{
+
+    Choice cbookname;
+
+    RemoveBook(){
+
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        setResizable(false);
+
+        JLabel labelbookname=new JLabel("Book Name");
+        labelbookname.setBounds(50,50,100,30);
+        labelbookname.setForeground(Color.BLACK);
+        add(labelbookname);
+
+        cbookname=new Choice();
+        cbookname.setBounds(200,50,100,30); 
+        add(cbookname);  
+
+        /*try{
+        Conn c=new Conn();
+            String query ="select * from bookdetails"; 
+            ResultSet rs= c.s.executeQuery(query);
+            while(rs.next()){
+                 
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Invalid Username and Password");
+                setVisible(false);
+            }
+      ;
+*/
+        
+        
+    
+   /* public static void main(String args[]){
+        new RemoveBook();
+    }
+}*/
